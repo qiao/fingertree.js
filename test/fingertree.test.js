4df60a8ace79533e5abfd157f544b041fb57c36c
Xueqiao Xu <xueqiaoxu@gmail.com>
2014-07-02 20:30:51 -0700

    add: test cases for peeking

--- test/fingertree.test.js
+++ test/fingertree.test.js
@@ -4,8 +4,31 @@ var FingerTree = require('..');
 
 describe('Finger Tree', function () {
 
+  var tree;
+
   it('should construct an empty tree when given an empty array', function () {
-    var tree = FingerTree.toTree([]);
+    tree = FingerTree.fromArray([]);
     tree.isEmpty().should.be.true;
+  });
+
+  it('should construct a tree containing initial elements', function () {
+    tree = FingerTree.fromArray([1, 2, 3]);
+    tree.isEmpty().should.be.false;
+  });
+
+  it('should be able to get the first element', function () {
+    tree = FingerTree.fromArray([]);
+    (tree.peekFirst() === null).should.be.true;
+
+    tree = FingerTree.fromArray([1, 2, 3]);
+    tree.peekFirst().should.eql(1);
+  });
+
+  it('should be able to get the last element', function () {
+    tree = FingerTree.fromArray([]);
+    (tree.peekLast() === null).should.be.true;
+
+    tree = FingerTree.fromArray([1, 2, 3]);
+    tree.peekLast().should.eql(3);
   });
 });
