0adf559519ef421ce34786090ec83a311aeb1d41
Xueqiao Xu <xueqiaoxu@gmail.com>
2014-07-07 16:03:32 -0700

    add: test for a thousand inserts

--- test/fingertree.test.js
+++ test/fingertree.test.js
@@ -5,6 +5,14 @@ var FingerTree = require('..');
 describe('Finger Tree', function () {
 
   var tree;
+  
+  function range(n) {
+    var a = [];
+    for (var i = 0; i < n; ++i) {
+      a.push(i);
+    }
+    return a;
+  }
 
   it('should construct an empty tree when given an empty array', function () {
     tree = FingerTree.fromArray([]);
@@ -14,6 +22,9 @@ describe('Finger Tree', function () {
   it('should construct a tree containing initial elements', function () {
     tree = FingerTree.fromArray([1, 2, 3]);
     tree.isEmpty().should.be.false;
+
+    tree = FingerTree.fromArray(range(1000));
+    tree.isEmpty().should.be.false;
   });
 
   it('should be able to get the first element', function () {
@@ -22,6 +33,9 @@ describe('Finger Tree', function () {
 
     tree = FingerTree.fromArray([1, 2, 3]);
     tree.peekFirst().should.eql(1);
+
+    tree = FingerTree.fromArray(range(1000));
+    tree.peekFirst().should.eql(0);
   });
 
   it('should be able to get the last element', function () {
@@ -30,5 +44,8 @@ describe('Finger Tree', function () {
 
     tree = FingerTree.fromArray([1, 2, 3]);
     tree.peekLast().should.eql(3);
+
+    tree = FingerTree.fromArray(range(1000));
+    tree.peekLast().should.eql(999);
   });
 });
