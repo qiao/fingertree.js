47eeb68299a3c3a8da74f0345b7d1acc7f964616
Xueqiao Xu <xueqiaoxu@gmail.com>
2014-07-14 14:32:16 -0700

    add: more tests, improving coverage to 84 %

--- test/fingertree.test.js
+++ test/fingertree.test.js
@@ -29,6 +29,9 @@ describe('Finger Tree', function () {
     var tree = FingerTree.fromArray([]);
     (tree.peekFirst() === null).should.be.true;
 
+    tree = FingerTree.fromArray([1]);
+    tree.peekFirst().should.eql(1);
+
     tree = FingerTree.fromArray([1, 2, 3]);
     tree.peekFirst().should.eql(1);
 
@@ -40,11 +43,50 @@ describe('Finger Tree', function () {
     var tree = FingerTree.fromArray([]);
     (tree.peekLast() === null).should.be.true;
 
+    tree = FingerTree.fromArray([1]);
+    tree.peekLast().should.eql(1);
+
     tree = FingerTree.fromArray([1, 2, 3]);
     tree.peekLast().should.eql(3);
 
     tree = FingerTree.fromArray(range(1000));
     tree.peekLast().should.eql(999);
+  });
+
+  it('should be able to add element to the front', function () {
+    var tree = FingerTree.fromArray([]);
+    tree = tree.addFirst(1);
+    tree.peekFirst().should.equal(1);
+
+    tree = FingerTree.fromArray([1]);
+    tree = tree.addFirst(0);
+    tree.peekFirst().should.equal(0);
+
+    tree = FingerTree.fromArray([1, 2, 3]);
+    tree = tree.addFirst(0);
+    tree.peekFirst().should.equal(0);
+
+    tree = FingerTree.fromArray(range(1000));
+    tree = tree.addFirst(-1);
+    tree.peekFirst().should.equal(-1);
+  });
+
+  it('should be able to add element to the end', function () {
+    var tree = FingerTree.fromArray([]);
+    tree = tree.addLast(1);
+    tree.peekLast().should.equal(1);
+
+    var tree = FingerTree.fromArray([1]);
+    tree = tree.addLast(2);
+    tree.peekLast().should.equal(2);
+
+    tree = FingerTree.fromArray([1, 2, 3]);
+    tree = tree.addLast(4);
+    tree.peekLast().should.equal(4);
+
+    tree = FingerTree.fromArray(range(1000));
+    tree = tree.addLast(1000);
+    tree.peekLast().should.equal(1000);
   });
 
   it('should be able to get a new tree with the first element removed', function () {
@@ -58,7 +100,11 @@ describe('Finger Tree', function () {
   });
 
   it('should be able to get a new tree with the last element removed', function () {
-    var tree = FingerTree.fromArray([1, 2, 3]).removeLast();
+    var tree = FingerTree.fromArray([1]).removeLast();
+    (tree.peekFirst() === null).should.be.true;
+    (tree.peekLast() === null).should.be.true;
+
+    tree = FingerTree.fromArray([1, 2, 3]).removeLast();
     tree.peekFirst().should.eql(1);
     tree.peekLast().should.eql(2);
 
@@ -68,7 +114,23 @@ describe('Finger Tree', function () {
   });
 
   it('should be able to concat two trees into one', function () {
-    var tree = FingerTree.fromArray([1, 2, 3]).concat(FingerTree.fromArray([4, 5, 6]));
+    var tree = FingerTree.fromArray([]).concat(FingerTree.fromArray([1, 2, 3]));
+    tree.peekFirst().should.eql(1);
+    tree.peekLast().should.eql(3);
+
+    tree = FingerTree.fromArray([1, 2, 3]).concat(FingerTree.fromArray([]));
+    tree.peekFirst().should.eql(1);
+    tree.peekLast().should.eql(3);
+
+    tree = FingerTree.fromArray([1]).concat(FingerTree.fromArray([2, 3]));
+    tree.peekFirst().should.eql(1);
+    tree.peekLast().should.eql(3);
+
+    tree = FingerTree.fromArray([1, 2]).concat(FingerTree.fromArray([3]));
+    tree.peekFirst().should.eql(1);
+    tree.peekLast().should.eql(3);
+
+    tree = FingerTree.fromArray([1, 2, 3]).concat(FingerTree.fromArray([4, 5, 6]));
     tree.peekFirst().should.eql(1);
     tree.peekLast().should.eql(6);
   });
@@ -82,8 +144,27 @@ describe('Finger Tree', function () {
   });
 
   it('should be able to be split into two halves given a predicate', function () {
-    var tree = FingerTree.fromArray(range(100));
-    var split = tree.split(function (x) {
+    var tree = FingerTree.fromArray([]);
+    var split = tree.split(function (x) { return true; });
+    split[0].isEmpty().should.be.true;
+    split[1].isEmpty().should.be.true;
+
+    tree = FingerTree.fromArray([1]);
+    split = tree.split(function (x) {
+      return x > 0;
+    });
+    split[0].measure().should.eql(1);
+    split[1].isEmpty().should.be.true;
+
+    tree = FingerTree.fromArray([1]);
+    split = tree.split(function (x) {
+      return x > 1;
+    });
+    split[0].isEmpty().should.be.true;
+    split[1].measure().should.eql(1);
+
+    tree = FingerTree.fromArray(range(100));
+    split = tree.split(function (x) {
       return x > 50;
     });
     split[0].measure().should.eql(50);
